22b7ad261093724c410185f7c3049a3fd75a5ee8
Mariusz <majuszt@o2.pl>
2021-01-10 12:24:29 +0100

    2.1

---
+++ src/pl/wsiz/podyplomowe/io/part2/Employee.java
@@ -0,0 +1,7 @@
+package pl.wsiz.podyplomowe.io.part2;
+
+public class Employee {
+    String firstname;
+    String lastname;
+    int age;
+}
---
+++ src/pl/wsiz/podyplomowe/io/part2/Program.java
@@ -0,0 +1,12 @@
+package pl.wsiz.podyplomowe.io.part2;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+
+        employee.firstname = "Jan";
+        employee.lastname = "Kowalski";
+        employee.age = 30;
+    }
+}
