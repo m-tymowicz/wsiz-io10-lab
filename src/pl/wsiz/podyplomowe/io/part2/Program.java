27c4153fe53b6009cf8161f1332b014045b9d0f7
Mariusz <majuszt@o2.pl>
2021-01-23 09:45:49 +0100

    2.2

--- src/pl/wsiz/podyplomowe/io/part2/Program.java
+++ src/pl/wsiz/podyplomowe/io/part2/Program.java
@@ -3,10 +3,16 @@ package pl.wsiz.podyplomowe.io.part2;
 public class Program {
 
     public static void main(String[] args) {
-        Employee employee = new Employee();
+        Employee employee1 = new Employee();
+        employee1.firstname = "Jan";
+        employee1.lastname = "Kowalski";
+        employee1.age = 32;
+        employee1.print();
 
-        employee.firstname = "Jan";
-        employee.lastname = "Kowalski";
-        employee.age = 32;
+        Employee employee2 = new Employee();
+        employee2.firstname = "Anna";
+        employee2.lastname = "Nowak";
+        employee2.age = 30;
+        employee2.print();
     }
 }
