49d65658672e94a9c43da8de6acd0a13157ec885
Mariusz <majuszt@o2.pl>
2021-02-06 09:48:56 +0100

    2.10 (if-else)

--- src/pl/wsiz/podyplomowe/io/part2/Program.java
+++ src/pl/wsiz/podyplomowe/io/part2/Program.java
@@ -1,8 +1,11 @@
 package pl.wsiz.podyplomowe.io.part2;
 
-public class Program {
+import java.util.Scanner;
 
+public class Program {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Company company = new Company();
 
         Employee employee1 = new Employee("Jan", "Kowalski", 32);
@@ -13,12 +16,34 @@ public class Program {
         company.addEmployee(employee2);
         company.addEmployee(employee3);
 
-        company.printEmployees();
+        int operation;
+        do {
+            System.out.println("Lista operacji:\n");
 
-        company.removeEmployee(40);
-        company.printEmployees();
+            System.out.println("1 - wypisz listę pracowników");
+            System.out.println("2 - dodaj pracownika");
+            System.out.println("3 - usuń pracownika");
+            System.out.println("9 - zakończ program\n");
 
-        company.removeEmployee(2);
-        company.printEmployees();
+            System.out.print("Podaj numer operacji: ");
+            operation = scanner.nextInt();
+
+            if (operation == 1) {
+                company.printEmployees();
+
+            } else if (operation == 2) {
+                company.addEmployee();
+
+            } else if (operation == 3) {
+                System.out.print("Podaj numer pracownika do usunięcia: ");
+                int employeeIndex = scanner.nextInt();
+                company.removeEmployee(employeeIndex);
+
+            } else if (operation != 9) {
+                System.out.println("Operacja o podanym numerze nie istnieje!");
+            }
+
+        } while (operation != 9);
+
     }
 }
